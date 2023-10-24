4b389e38fb39408f57cca69caff11c4cd7edfb2f
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-10-24 05:27:35 +0000

    x

---
+++ src/components/Data.js
@@ -0,0 +1,72 @@
+import React,{useState} from 'react'
+import axios from 'axios'
+
+function Userdata(){
+   let [isClicked,setClicked] = useState(false)
+    let [datas,setDatas] = useState([]);
+
+    function handleUser(){
+        axios.get("https://reqres.in/api/users")
+        .then((data)=>{
+            console.log(data.data.data)
+            setDatas(data.data.data);
+            if(isClicked === false){
+                setClicked(true)
+            }
+            else{
+                setClicked(false)
+            }
+        })
+        .catch((error)=>{
+            alert(error)
+        })
+        
+    }
+
+    return (
+        <div>
+            <div className='btn-head'>
+                <h2>Blue Whales</h2>
+                <button onClick={handleUser}  className='btn'>Get User List</button>
+            </div>
+            
+                <table >
+                    <thead>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Avatar</th>
+                    </thead>
+               {
+
+                isClicked ? (
+                        <tbody>
+
+                            {
+                                datas.map((data)=>{
+                                    return (
+                                        <tr>
+                                            <td>{data.first_name}</td>
+                                            <td>{data.last_name}</td>
+                                            <td>{data.email}</td>
+                                            <td><img src={data.avatar}></img></td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>  
+
+                    )
+                    :
+                    (
+                        <tr>
+                            <td className='no-data'>No data found to display</td>
+                        </tr>
+                    )
+               } 
+            </table>
+            
+        </div>
+    )
+}
+export default Userdata
